ca48fce88dc6ab756760f23495e35f15d59e5464
Muharrem Tozan <muharremtozan@hotmail.com>
2024-11-07 19:42:28 +0300

    Initial commit

---
+++ src/main/java/com/prodmate/ProdMate/ProdMateApplication.java
@@ -0,0 +1,13 @@
+package com.prodmate.ProdMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prodmate/ProdMate/ProdMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodmate.ProdMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
